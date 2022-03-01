3a9e48ae7b0b9f780d690589e307568236c8ac98
Tharun <77895048+tharun-vemula@users.noreply.github.com>
2022-03-01 23:54:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const parser = require('./routes/parse');
 
 const app = express();
 
 app.use(bodyParser.json()); //supports parsing of application/json
 
-
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));  // supports parsing of x-www-form-urlencoded <form>
 
 
 //headers to avoid CORS errors
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-     res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
-app.get('/', (req, res) => {
-    res.send('Server Running!');
-});
-        
+
 app.use(parser);
 
 app.listen(process.env.PORT || 8080);
