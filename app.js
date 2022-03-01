d9d7bb15ee18e05ae73e475bca5f6e3f6dd9aa08
Tharun <77895048+tharun-vemula@users.noreply.github.com>
2022-03-01 18:13:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));  // supports parsing of x-www-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+     res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
